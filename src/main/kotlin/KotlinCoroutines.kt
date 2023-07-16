64aa15be8ad91340e92e167799777bc6cb9661b5
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-16 14:22:42 +0530

    run blocking

--- src/main/kotlin/KotlinCoroutines.kt
+++ src/main/kotlin/KotlinCoroutines.kt
@@ -1,20 +1,25 @@
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
 fun main() {
-    println("Main program starts: ${Thread.currentThread().name}")
 
-GlobalScope.launch {    //creates a background coroutine that runs on a background thread //let's say Thread: T1
-    println("Fake work starts: ${Thread.currentThread().name}")
+runBlocking {
+    println("Main program starts: ${Thread.currentThread().name}")
+    GlobalScope.launch {    //creates a background coroutine that runs on a background thread //let's say Thread: T1
+        println("Fake work starts: ${Thread.currentThread().name}")
 //    Thread.sleep(1000) //pretend doing some work...maybe file upload
-    delay(1000) //better than thread.sleep //Coroutine is suspended but thread T1 is free(not blocked)
-    println("Fake work finished: ${Thread.currentThread().name}")
-}
+        delay(1000) //better than thread.sleep //Coroutine is suspended but thread T1 is free(not blocked)
+        println("Fake work finished: ${Thread.currentThread().name}")
+    }
 
     //the below thread.slepp blocks the current main thread and wait for the coroutine to finish(practically not the right way to wait)
-    Thread.sleep(2000)
+//    Thread.sleep(2000)
+
+    delay(1000) //main thread: wait for coroutine to finish the execution(practically not the right way to wait)
     println("Main program ends: ${Thread.currentThread().name}")
 
+}
 }
