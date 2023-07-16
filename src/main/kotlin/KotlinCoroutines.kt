622fecfc18cbf29b71de2666aa655c9d23cd5859
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-16 16:21:24 +0530

    async and runblocking

--- src/main/kotlin/KotlinCoroutines.kt
+++ src/main/kotlin/KotlinCoroutines.kt
@@ -4,11 +4,12 @@ import kotlin.concurrent.thread
 fun main() = runBlocking{
 
     println("Main program starts: ${Thread.currentThread().name}")
-    val job: Job = launch {    //creates a background coroutine that runs on the main thread
+    val jobDeferred: Deferred<Int> = async {    //creates a background coroutine that runs on the main thread
         println("Fake work starts: ${Thread.currentThread().name}")
 //    Thread.sleep(1000) //pretend doing some work...maybe file upload
-        mySuspendFun(1000) //better than thread.sleep //Coroutine is suspended but thread T1 is free(not blocked)
+        delay(1000) //better than thread.sleep //Coroutine is suspended but thread T1 is free(not blocked)
         println("Fake work finished: ${Thread.currentThread().name}") //either main thread or some other thread
+        15
     }
 
     //the below thread.slepp blocks the current main thread and wait for the coroutine to finish(practically not the right way to wait)
@@ -16,12 +17,12 @@ fun main() = runBlocking{
 
 //    mySuspendFun(1000) //main thread: wait for coroutine to finish the execution(practically not the right way to wait)
     //you can also cancel a coroutine using job.cancel()
-    job.join() //join function will wit for the coroutine to finish the execution only after which the next statement will be exceutred
+    val num: Int = jobDeferred.await() //join function will wit for the coroutine to finish the execution only after which the next statement will be exceutred
     println("Main program ends: ${Thread.currentThread().name}")
 
 }
 
-suspend fun mySuspendFun(time: Long)
+suspend fun myOwnSuspendFun()
 {
-    delay(time)
+    delay(1000)
 }
