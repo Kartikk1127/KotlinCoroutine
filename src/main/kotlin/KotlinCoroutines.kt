2bc6e659e347f56bb7144ef3df36640e070ae891
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-16 13:20:36 +0530

    coroutine basics

--- src/main/kotlin/KotlinCoroutines.kt
+++ src/main/kotlin/KotlinCoroutines.kt
@@ -1,13 +1,20 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 
 fun main() {
     println("Main program starts: ${Thread.currentThread().name}")
 
-thread {    //creates a background thread(worker thread)
+GlobalScope.launch {    //creates a background coroutine that runs on a background thread //let's say Thread: T1
     println("Fake work starts: ${Thread.currentThread().name}")
-    Thread.sleep(1000) //pretend doing some work...maybe file upload
+//    Thread.sleep(1000) //pretend doing some work...maybe file upload
+    delay(1000) //better than thread.sleep //Coroutine is suspended but thread T1 is free(not blocked)
     println("Fake work finished: ${Thread.currentThread().name}")
 }
+
+    //the below thread.slepp blocks the current main thread and wait for the coroutine to finish(practically not the right way to wait)
+    Thread.sleep(2000)
     println("Main program ends: ${Thread.currentThread().name}")
 
 }
