17591c4cbfc5d243b88faab395a2a5914424b225
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-17 13:09:54 +0530

    with timeout

--- src/main/kotlin/KotlinCoroutines.kt
+++ src/main/kotlin/KotlinCoroutines.kt
@@ -5,28 +5,20 @@ fun main() = runBlocking{
 
     println("Main program starts: ${Thread.currentThread().name}")
 
-    val job = launch(Dispatchers.Default) {
+    withTimeout(2000){ //if this coroutine fails to execute within 2000 ms then this function(withTimeout) will throw TimeoutCancellationException
         try{
-            for(i in 0..500){
+            for (i in 0..500){
                 println("$i.")
-                delay(5)
+                delay(500)
             }
         }
-        catch (ex: CancellationException){
-            print("\nException caught safely: ${ex.message}")
+        catch (ex: TimeoutCancellationException){
+            println("Exception handled successfully")
         }
         finally {
-            withContext(NonCancellable){
-                delay(1000) //generally we don't use suspending function in finally
-                print("\nClose resources in finally")
-            }
+            println("Code ends now")
         }
     }
-
-    delay(10) //printing few values before we cancel
-    job.cancel(CancellationException("My crash message"))
-    job.join() //waits for coroutine to finish
-//    job.cancelAndJoin() you can also use this function instead of the above separate functions
 
     println("\nMain program ends: ${Thread.currentThread().name}")
 
