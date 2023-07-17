cd0aaaa93e1d0533331ad149b812eaaace0293d1
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-17 12:56:47 +0530

    handling exceptions

--- src/main/kotlin/KotlinCoroutines.kt
+++ src/main/kotlin/KotlinCoroutines.kt
@@ -5,16 +5,26 @@ fun main() = runBlocking{
 
     println("Main program starts: ${Thread.currentThread().name}")
 
-    val job = launch {
-        for(i in 0..500){
-            println("$i.")
-//            delay(50)
-            yield() //or use delay() or any other suspending function as per your need
+    val job = launch(Dispatchers.Default) {
+        try{
+            for(i in 0..500){
+                println("$i.")
+                delay(5)
+            }
+        }
+        catch (ex: CancellationException){
+            print("\nException caught safely: ${ex.message}")
+        }
+        finally {
+            withContext(NonCancellable){
+                delay(1000) //generally we don't use suspending function in finally
+                print("\nClose resources in finally")
+            }
         }
     }
 
     delay(10) //printing few values before we cancel
-    job.cancel()
+    job.cancel(CancellationException("My crash message"))
     job.join() //waits for coroutine to finish
 //    job.cancelAndJoin() you can also use this function instead of the above separate functions
 
